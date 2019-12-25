a4d5b1fe0eb2e14413018e13646e1c4d2aae9c99
alphaxyz <iloca95@gmail.com>
2019-12-25 23:27:25 +0100

    backend - first commit

---
+++ backend/movies/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    tmdb_id = models.CharField(primary_key=True, max_length=50)
+    adult = models.BooleanField(default=False)
+    belongs_to_collection = models.IntegerField(null=True, blank=True)
+    budget = models.IntegerField(null=True, blank=True)
+    homepage = models.CharField(null=True, blank=True, max_length=100)
+    imdb_id = models.CharField(null=True, blank=True, max_length=100)
+    original_language = models.CharField(blank=True, null=True, max_length=100)
+    original_title = models.CharField(blank=True, null=True, max_length=100)
+    overview = models.TextField()
+    popularity = models.DecimalField(max_digits=10, null=True, decimal_places=6, blank=True)
+    backdrop_path = models.CharField(null=True, blank=True, max_length=100)
+    poster_path = models.CharField(null=True, blank=True, max_length=100)
+    release_date = models.DateField(blank=True, null=True)
+    revenue = models.IntegerField(null=True, blank=True)
+    runtime = models.IntegerField(null=True, blank=True)
+    status = models.CharField(blank=True, null=True, max_length=100)
+    tagline = models.CharField(blank=True, null=True, max_length=100)
+    title = models.CharField(max_length=100)
+    video = models.BooleanField()
+    vote_average = models.DecimalField(max_digits=3, null=True, decimal_places=1 , blank=True, default=0)
+    vote_count = models.IntegerField(null=True, blank=True)
+    last_updated = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.tmdb_id, self.title)
---
+++ backend/movies/views.py
@@ -0,0 +1,103 @@
+import requests
+from .models import Movie
+from django.conf import settings
+from .serializers import MovieSerializer
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from pprint import pprint #debug
+
+# Create your views here.
+class MovieListCreate(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    http_method_names = ['get', 'head']
+
+
+@api_view(['GET'])
+def get_movie(request, tmdb): 
+    """Returns movie's informations.
+    Args:
+        tmdb: movie's id.
+
+    Returns:
+        movie: a JSON containing the movie's information.
+    """
+    payload = { 'api_key': settings.TMDB_KEY, 'language': 'en-US'}
+    url = settings.TMDB_URL.format(endpoint='movie')
+    url += str(tmdb)
+    r = requests.get(url=url, params=payload)
+    print(r.url)
+    if not r:
+        raise NotFound(detail='Movie not found.')
+
+    return Response(r.json())
+
+@api_view(['GET'])
+def search_movie(request, title):
+    """Search a movie based on a title.
+    Args:
+        title: the search term, title of the desired movie.
+
+    Returns:
+        movies: a JSON containing a list of search results.
+    """
+    payload = {'api_key': settings.TMDB_KEY, 'language': 'en-US', 'query': title, 'page': 1}
+    url = settings.TMDB_URL.format(endpoint='search/movie')
+
+    r = requests.get(url=url, params=payload)
+
+    return Response(r.json())
+
+@api_view(['GET'])
+def get_upcoming(request):
+    """
+    Returns:
+        movie: a JSON containing upcoming movies.
+    """ 
+    payload = { 'api_key': settings.TMDB_KEY, 'language': 'en-US', 'page': 1}
+    url = settings.TMDB_URL.format(endpoint='movie')
+    url += 'upcoming'
+    r = requests.get(url=url, params=payload)
+    print(r.url)
+    if not r:
+        raise NotFound(detail='Movies not found.')
+
+    return Response(r.json())
+
+@api_view(['GET'])
+def get_now_playing(request): 
+    """
+    Returns:
+        movie: a JSON containing nowplaying movies.
+    """ 
+    payload = { 'api_key': settings.TMDB_KEY, 'language': 'en-US', 'page': 1}
+    url = settings.TMDB_URL.format(endpoint='movie')
+    url += 'now_playing'
+    r = requests.get(url=url, params=payload)
+    print(r.url)
+    if not r:
+        raise NotFound(detail='Movies not found.')
+
+    return Response(r.json())
+
+@api_view(['GET'])
+def get_credits(request, tmdb): 
+    """Returns movie's credits.
+    Args:
+        tmdb: movie's id.
+
+    Returns:
+        movie: a JSON containing the movie's credits.
+    """
+    payload = { 'api_key': settings.TMDB_KEY}
+    url = settings.TMDB_URL.format(endpoint='movie')
+    url += str(tmdb)
+    url += '/credits'
+    r = requests.get(url=url, params=payload)
+    print(r.url)
+    if not r:
+        raise NotFound(detail='Movies not found.')
+
+    return Response(r.json())
